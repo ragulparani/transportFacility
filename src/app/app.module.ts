448b49a2c9c5fc307038d22af12e9cd6c44a0de9
ragulparani <ragul.paranitharan@enquero.com>
2020-08-17 08:40:01 +0530

    Add Css and less file changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { registerLocaleData } from "@angular/common";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
+
+
+/** config angular i18n **/
+import en from "@angular/common/locales/en";
+registerLocaleData(en);
+
+/** config ng-zorro-antd i18n **/
+import { NZ_I18N, en_US } from "ng-zorro-antd/i18n";
 import { AddVehicleComponent } from './add-vehicle/add-vehicle.component';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,9 +32,10 @@ import { AddVehicleComponent } from './add-vehicle/add-vehicle.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NzSelectModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { NzMessageService } from "ng-zorro-antd/message";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  emp_id : number;
+  constructor(public login : LoginService, public message : NzMessageService, public router : Router) { }
 
   ngOnInit() {
   }
 
+  logIn(){
+    this.login.login(this.emp_id) ? this.router.navigate(['/home']) : this.message.error('Employee id does not exist')
+  }
 }
